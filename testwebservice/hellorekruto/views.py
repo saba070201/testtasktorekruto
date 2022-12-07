403de9d8a584301128142f2e1378cb8a23c4f7f1
миша сабадаш <misasabadas@mishas-MacBook-Air.local>
2022-12-07 03:02:40 +0300

    finish1)

--- testwebservice/hellorekruto/views.py
+++ testwebservice/hellorekruto/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse,QueryDict
 def hello(request):
-    # request=request.GET(QueryDict('a=1&a=2&c=3'))
-    # if request.method=='GET':
-    #     return render('hellorekruto/hello.html')
     name = request.GET.get('name','rekruto')
     message = request.GET.get('message','hello')
     return render(request,'hellorekruto/hello.html',{'message':message,'name':name})
 def home(request):
     return render(request,'hellorekruto/home.html')
 
-# Create your views here.
