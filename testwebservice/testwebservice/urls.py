f38d0a7aace12187af5a601daddf4198754f262b
миша сабадаш <misasabadas@mishas-MacBook-Air.local>
2022-12-07 03:01:28 +0300

    finish

--- testwebservice/testwebservice/urls.py
+++ testwebservice/testwebservice/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from hellorekruto import views
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('hello/',views.hello,name='hello'),
+    path('',views.home,name='home'),
 ]
